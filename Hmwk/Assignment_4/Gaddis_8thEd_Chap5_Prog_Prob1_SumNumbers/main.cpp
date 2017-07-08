a524c1988eef83681952244b12b9d0f51839edbc
jventura10 <jventura1097@gmail.com>
2017-07-08 00:52:46 -0700

    Signed-off-by: jventura10 <jventura1097@gmail.com>

---
+++ Hmwk/Assignment_4/Gaddis_8thEd_Chap5_Prog_Prob1_SumNumbers/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Javier Ventura
+ * Purpose: Add all numbers up from 1 to the number entered.
+ * Created on July 7, 2017, 11:30 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    //Declare Variables
+    int input,sum=0; //Integer from 1 to 50
+    
+    //Display Purpose and Prompt for Data
+    cout<<"This Program adds all the numbers from 1 to";
+    cout<<" the number entered."<<endl;
+    cout<<"Please enter an integer from 1 to 50.";
+    cout<<" Press enter when done."<<endl;
+    cin>>input;
+    
+    //Create Loop
+    for(int num=1;num<=input;num++){
+        sum += num;
+    
+    }
+    
+    //Display Total
+    cout<<"The total sum of integers from 1 to "<<input;
+    cout<<" is "<<sum<<"."<<endl;
+
+    return 0;
+}
+
