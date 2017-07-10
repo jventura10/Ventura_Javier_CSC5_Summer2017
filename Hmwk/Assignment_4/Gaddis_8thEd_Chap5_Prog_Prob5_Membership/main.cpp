7b0dd73bf45cbdf6c5b0e3d6531a6bd11d3ac21c
jventura10 <jventura1097@gmail.com>
2017-07-09 19:40:36 -0700

    Signed-off-by: jventura10 <jventura1097@gmail.com>

--- Hmwk/Assignment_4/Gaddis_8thEd_Chap5_Prog_Prob5_Membership/main.cpp
+++ Hmwk/Assignment_4/Gaddis_8thEd_Chap5_Prog_Prob5_Membership/main.cpp
@@ -6,20 +6,25 @@
  */
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main(int argc, char** argv) {
     //Declare Variables
     int year=1;      //Number of years 1-6
-    float perInc=100.0f; //Percent fees will increase a year
-    float annMem=2500;
+    float perInc=.04; //Percent fees will increase each year
+    float totInc;  //Total amount fee will increase in dollars each year
+    float annMem=2500; //0 year the membership is $2500
 
     //Display Purpose
-    cout<<"The Current Annual Membership Fee at the country club is $2500.";
+    cout<<"The Current Annual Membership Fee at the country club is $2500. ";
+    cout<<"After a 4% increase each year: ";
     cout<<endl;
     
-    for(annMem=2600;annMem<=3100; annMem += perInc){
+    cout<<fixed<<setprecision(2)<<showpoint;
+    for(annMem=2600;annMem<=3200; annMem += totInc){
+        totInc=perInc*annMem;
         cout<<"In "<<year<<" year(s) the annual membership will be $"<<annMem;
         cout<<"."<<endl;
         year++;
