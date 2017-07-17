f19aa2dc81d7322b5f25f18963900fa48003ed61
jventura10 <jventura1097@gmail.com>
2017-07-16 22:32:02 -0700

    Signed-off-by: jventura10 <jventura1097@gmail.com>

---
+++ Proj/Project 1/21_Card_Game/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Javier Venturas
+ * Purpose: 21 Card Game, Dealer is COM 
+ * Created on July 14, 2017, 10:20 PM
+ */
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    //Declare Variables
+    int nUsers;  //Number of Users
+    
+
+    return 0;
+}
+
