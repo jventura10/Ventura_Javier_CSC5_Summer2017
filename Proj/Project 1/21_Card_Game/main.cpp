eea9f737c2873f2b253cf97113c499393a84561d
jventura10 <jventura1097@gmail.com>
2017-07-18 00:43:20 -0700

    Signed-off-by: jventura10 <jventura1097@gmail.com>

--- Proj/Project 1/21_Card_Game/main.cpp
+++ Proj/Project 1/21_Card_Game/main.cpp
@@ -1,26 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /* 
  * File:   main.cpp
- * Author: Javier Venturas
+ * Author: Javier Ventura
  * Purpose: 21 Card Game, Dealer is COM 
  * Created on July 14, 2017, 10:20 PM
  */
 
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main(int argc, char** argv) {
-    //Declare Variables
-    int nUsers;  //Number of Users
+    //Set Random Seed
+    srand(static_cast<unsigned int>(time(0)));
     
-
+    //Declare Variables
+    int init1;
+    int init2;
+    unsigned int sum1;
+    float bet;
+    char hit;
+    int card1;
+    
+    init1=rand()%10+1;
+    init2=rand()%10+1;
+    card1=rand()%10+1;
+    sum1=init1+init2;
+    
+    cout<<"BlackJack Table!"<<endl;
+    cout<<endl;
+    
+    cout<<"Place Your bet: $";
+    cin>>bet;
+    
+    cout<<"Your cards: "<<init1<<" "<<init2<<endl;
+    cout<<"Sum: "<<sum1<<endl;
+    if(sum1==21){
+        cout<<"You Win!!!"<<endl;
+        cout<<"Earnings: $"<<bet*2.0<<endl;
+    }
+    
+    cout<<endl;
+    cout<<"Do you want another card?"<<endl;
+    cin>>hit;
+    while(hit=='Y'){
+        cout<<"Your Cards: "<<init1<<" "<<init2<<" "<<card1<<endl;
+        sum1=init1+init2+card1;
+        cout<<"Sum: "<<sum1<<endl;
+        cin>>hit;
+        if(sum1<21){
+            
+        }
+    }
+   
+    
+    
     return 0;
 }
 
